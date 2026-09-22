e3183cc2a2a7bbcc645a064351d2caaec49fa529
João Pedro Torrano Dias <joaopedrodias028@gmail.com>
2026-09-22 19:11:29 -0300

    test: add test scaffolding for controllers

---
+++ test/presentation/controller/auto_controller_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'package:bullsapp/data/repositories/robot_command_port.dart';
+import 'package:bullsapp/presentation/controllers/auto_controller.dart';
+
+class MockRobotCommandPort extends Mock implements RobotCommandPort{}
+
+void main() {}
